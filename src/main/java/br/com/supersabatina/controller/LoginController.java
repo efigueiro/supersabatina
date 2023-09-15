ab20b5df1e1ebc3d4dbcce1225b0d849bd619627
evers <evers@Workstation>
2023-09-15 09:44:28 -0300

    Added login controller

--- src/main/java/br/com/supersabatina/controller/LoginController.java
+++ src/main/java/br/com/supersabatina/controller/LoginController.java
@@ -1,29 +1,62 @@
 package br.com.supersabatina.controller;
 
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+
+import br.com.supersabatina.model.entity.User;
+import br.com.supersabatina.service.LoginService;
+import br.com.supersabatina.util.Messenger;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-import br.com.supersabatina.model.dao.BaseDao;
 
 @WebServlet("/loginController")
 public class LoginController extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
-       
-    public LoginController() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public LoginController() {
+		super();
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String email = (String) request.getParameter("txtEmail");
+		String password = (String) request.getParameter("txtPassword");
+
+		User user = new User();
+		User authenticated = new User();
+		LoginService loginService = new LoginService();
+
+		user.setEmail(email);
+		user.setPassword(password);
+
+		authenticated = loginService.login(user);
+
+		if (StringUtils.isNotEmpty(email)) {
+			// add to session and redirect to the system
+			System.out.println(authenticated.getEmail());
+			System.out.println(authenticated.getPassword());
+			for (String message : Messenger.messageList) {
+				System.out.println(message);
+			}
+			System.out.println(Messenger.success);
+		} else {
+			System.out.println(authenticated.getEmail());
+			System.out.println(authenticated.getPassword());
+			for (String message : Messenger.messageList) {
+				System.out.println(message);
+			}
+			System.out.println(Messenger.success);
+		}
+	}
 }
---
+++ src/main/java/br/com/supersabatina/service/LoginService.java
@@ -0,0 +1,40 @@
+package br.com.supersabatina.service;
+
+import org.apache.commons.lang3.StringUtils;
+
+import br.com.supersabatina.model.dao.UserDao;
+import br.com.supersabatina.model.entity.User;
+import br.com.supersabatina.util.Encrypt;
+import br.com.supersabatina.util.Messenger;
+
+public class LoginService {
+
+	public User login(User user) {
+
+		User authenticated = new User();
+		UserDao userDao = new UserDao();
+		String comparePassword = "";
+
+		// Get user password, encrypt and store it into compare password variable
+		if (StringUtils.isNotEmpty(user.getPassword())) {
+			comparePassword = Encrypt.encrypt(user.getPassword());
+		}
+
+		// Find user
+		authenticated = userDao.retrieveByEmail(user);
+
+		// Check password match
+		if (StringUtils.isNotEmpty(authenticated.getPassword())) {
+			
+			if (comparePassword.equals(authenticated.getPassword())) {
+				return authenticated;
+			}else {
+				Messenger.addWarningMessage("Usuário ou password incorreto.");
+				Messenger.setSuccessFalse();
+			}
+			
+		}
+		
+		return null;
+	}
+}
